1a1bca6678905f3e95beb28e4c3afbc0a8e8f7ef
Arvind <pingarvindforquestions@gmail.com>
2026-07-08 17:33:34 +0000

    fix(main.py): added a new function for creating an app

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,22 +6,24 @@ from fastapi.middleware.cors import CORSMiddleware
 from backend.app.api.router import register_v1_api_routes
 from di import load_all_deps
 
-origins = ["*"]
 
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-register_v1_api_routes(app=app)
+def create_app() -> FastAPI:
+    """Entrypoint function for app."""
+    load_all_deps()
 
-load_all_deps()
+    origins = ["*"]
+
+    app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    register_v1_api_routes(app=app)
+
+    return app
 
 
-@app.get("/api")
-async def hello_world() -> dict[str, str]:
-    """Health check endpoint."""
-    return {"Hello": "World"}
+app = create_app()
